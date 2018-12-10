a8cb45a6a6812e6ce12a3f24f2eb426d2a1e456a
Sergey Matveev <s_matveev95@mail.ru>
2018-12-10 12:22:27 +0200

    'kakjeyaustal'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import mysql from 'mysql'
+import { join } from 'path';
 const app = express();
 const port = 8080;
 
@@ -162,6 +163,8 @@ app.post('/admin/addcourse', (req, res) =>{
 	})
 })
 
+//ADD THEME
+
 app.post('/admin/addtheme', (req, res) =>{
 	const theme = req.body.split(',')
 	con.query(`SELECT * FROM themes`, (err, data) => {
@@ -206,8 +209,10 @@ app.post('/admin/addtheme', (req, res) =>{
 	})
 })
 
+//ADD LESSON
+
 app.post('/admin/addlessons', (req, res) =>{
-		const lesson = req.body.split(',')
+		const lesson = req.body.split('----')
 		const empty = {
 			statusEror: true,
 			message:`Ошибка: пустое значение`
@@ -225,6 +230,8 @@ app.post('/admin/addlessons', (req, res) =>{
 			res.setHeader('Content-Type', 'application/json');
 			res.send(ok)
 })
+
+//ADD QST
 
 app.post('/admin/addqst', (req, res) =>{
 	const qst = req.body.split(',')
@@ -268,6 +275,8 @@ app.post('/admin/addqst', (req, res) =>{
 		}
 	})
 })
+
+//ADD TEST
 
 app.post('/admin/addtest', (req, res) =>{
 	const test = req.body.split(',')
@@ -313,6 +322,8 @@ app.post('/admin/addtest', (req, res) =>{
 	})
 })
 
+//ADD QST ANSWER
+
 app.post('/admin/addqstanswer', (req, res) =>{
 	const qa = req.body.split(',')
 	con.query(`SELECT * FROM qstanswers`, (err, data) => {
@@ -336,7 +347,7 @@ app.post('/admin/addqstanswer', (req, res) =>{
 			res.send(empty)
 			return;
 		}
-		let rule = false
+		/* let rule = false
 		for (let i = 0; i < arr.length; i++) {
 			if (qa[1] !== arr[i]) {
 				rule = true
@@ -344,18 +355,20 @@ app.post('/admin/addqstanswer', (req, res) =>{
 				rule = false
 				break
 			}
-		}
-		if (rule) {
+		} */
+		// if (rule) {
 			con.query(`INSERT INTO qstanswers (qst, qstAnswer, rqst) VALUE("${qa[0]}", "${qa[1]}", "${qa[2]}")`);
 			res.setHeader('Content-Type', 'application/json');
-			res.send(ok)
-		} else {
+			res.send(ok) 
+		/* } else { 
 			res.setHeader('Content-Type', 'application/json');
 			res.send(fail)
-		}
-		rule = false;
+		} 
+		rule = false; */
 	})
 })
+
+//ADD TEST ANSWER
 
 app.post('/admin/addtestanswer', (req, res) =>{
 	const ta = req.body.split(',')
@@ -380,7 +393,7 @@ app.post('/admin/addtestanswer', (req, res) =>{
 			res.send(empty)
 			return;
 		}
-		let rule = false
+		/* let rule = false
 		for (let i = 0; i < arr.length; i++) {
 			if (ta[1] !== arr[i]) {
 				rule = true
@@ -389,17 +402,19 @@ app.post('/admin/addtestanswer', (req, res) =>{
 				break
 			}
 		}
-		if (rule) {
+		if (rule) { */
 			con.query(`INSERT INTO testanswers (test, testAnswer, rtest) VALUE("${ta[0]}", "${ta[1]}", "${ta[2]}")`);
 			res.setHeader('Content-Type', 'application/json');
 			res.send(ok)
-		} else {
+		/* } else {
 			res.setHeader('Content-Type', 'application/json');
 			res.send(fail)
 		}
-		rule = false;
+		rule = false; */
 	})
 })
+
+//SHOW LESSONS
 
 app.post('/admin/showlesson', (req, res) =>{
 	con.query(`SELECT lesson FROM lessons WHERE theme='${req.body}'`, (err, data) => {
@@ -417,6 +432,8 @@ app.post('/admin/showlesson', (req, res) =>{
 		}
 	})
 })
+
+//SHOW QUEST
 
 app.post('/admin/showquest', (req, res) =>{
 	con.query(`SELECT qstAnswer, rqst  FROM qstAnswers WHERE qst='${req.body}'`, (err, data) => {
@@ -436,6 +453,8 @@ app.post('/admin/showquest', (req, res) =>{
 	})
 })
 
+//SHOW TEST
+
 app.post('/admin/showtest', (req, res) =>{
 	con.query(`SELECT testAnswer, rtest  FROM testAnswers WHERE test='${req.body}'`, (err, data) => {
 
@@ -453,6 +472,8 @@ app.post('/admin/showtest', (req, res) =>{
 		}
 	})
 })
+
+//DELETE ALL
 
 app.post('/admin/delete', (req, res) =>{
 	const reqArr = req.body.split(',')
@@ -478,6 +499,9 @@ app.post('/admin/delete', (req, res) =>{
 		}
 	})
 })
+
+//REGISTRATION
+
 app.post('/registration', (req, res) =>{
 	const reqArr = req.body.split(',')
 	con.query(`SELECT login FROM users`, (err, data) => {
@@ -493,15 +517,21 @@ app.post('/registration', (req, res) =>{
 			statusEror: true,
 			message:`Ошибка: "${reqArr[0]}" уже существует`
 		}
+		const login = data.map((el)=>{
+			return el.login
+		})
+		
 		let rule = false
-		for (let i = 0; i < data.length; i++) {
-			if (reqArr[0] !== data[i]) {
+		for (let i = 0; i < login.length ; i++) {
+			
+			if (reqArr[0] != login[i]) {
 				rule = true
 			} else {
 				rule = false
 				break
 			}
 		}
+		
 		if (reqArr[2] !== reqArr [3]) {
 			res.setHeader('Content-Type', 'application/json');
 			res.send(fail)
@@ -509,16 +539,18 @@ app.post('/registration', (req, res) =>{
 			res.setHeader('Content-Type', 'application/json');
 			res.send(empty)
 		} else if(rule) {
-			res.setHeader('Content-Type', 'application/json');
-			res.send(fail)
-		} else {
 			con.query(`INSERT INTO users (login, password, groupe) VALUE("${reqArr[0]}", "${reqArr[2]}", "${reqArr[1]}")`)
 			res.setHeader('Content-Type', 'application/json');
 			res.send(ok)
+		} else {
+			res.setHeader('Content-Type', 'application/json');
+			res.send(fail)
 		}
-		rule = false;
+		
 	})
 })
+
+//ENTER
 
 app.post('/enter', (req, res) =>{
 	const reqArr = req.body.split(',')
@@ -527,7 +559,6 @@ app.post('/enter', (req, res) =>{
 		const arrPass = data.map((el)=> {
 			return el.password
 		}) 
-		console.log(data[0])
 		const empty = {
 			statusEror: true,
 			message:`Ошибка: заполните все поля`
@@ -555,6 +586,131 @@ app.post('/enter', (req, res) =>{
 		}
 	})
 })
+
+app.post('/getreeting', (req, res) =>{
+	con.query(`SELECT courseName FROM courses`, (err, data) =>{
+		const course = data.map((el)=>{
+			return el.courseName
+		})
+		res.setHeader('Content-Type', 'application/json');
+		res.send(course)
+	})
+	
+})
+
+app.post('/addcourse', (req, res) =>{
+	const reqArr = req.body.split(',')
+	con.query(`SELECT * FROM usercourse`, (err, data) =>{
+		const course = data.map((el)=>{
+			return el.course
+		})
+
+		let rule = false
+		 for (let i = 0; i < course.length ; i++) {
+			if (reqArr[0] != course[i]) {
+				rule = true
+			} else {
+				rule = false
+				break
+			}
+		}
+		if (rule) {
+			console.log('add')
+			con.query(`INSERT INTO usercourse (login, course, testing, completing) VALUE("${reqArr[1]}", "${reqArr[0]}", "0", "0")`)
+		} 
+	})
+})
+	
+
+app.post('/gettesting', (req, res) =>{
+	const reqArr = req.body.split(',')
+	con.query(`SELECT testing FROM usercourse WHERE course="${reqArr[1]}"`, (err, data)=>{
+		let body = []
+		if (data[0].testing == '0') {
+			con.query(`SELECT * FROM qstanswers`, (err, data)=>{
+				body[1] = data
+			})
+			con.query(`SELECT * FROM qst`, (err, data)=>{
+				body[0] = data.map((el)=>{
+					return el.qst
+				})
+				body[2] = data.map((el)=>{
+					return el.theme
+				})
+				res.setHeader('Content-Type', 'application/json');
+				res.send(body)
+			})
+		} else {
+			res.setHeader('Content-Type', 'application/json');
+			res.send(['redirect'])
+		}
+	})
+})
+
+app.post('/endtesting', (req, res) =>{
+	const reqArr = req.body.split(',')
+	con.query(`UPDATE usercourse SET testing="1" WHERE course="${reqArr[1]}"`)
+	con.query(`SELECT theme FROM themes`, (err, data)=>{
+		const theme = data.map((el)=>{
+			return el.theme
+		})
+		const r = Number(reqArr[0])
+		if (r !== 0) {
+			const stheme = theme.splice(0, r)
+			stheme.forEach(element => {
+				con.query(`SELECT theme FROM usersthemes`, (err, data)=>{
+					const theme = data.map((el)=>{
+						return el.theme
+					})
+					theme.forEach((element1, i) => {
+						if (element1 != theme[i]) {
+							console.log('addr')
+							con.query(`INSERT INTO usersthemes (course, theme) VALUE("${reqArr[1]}", "${element1}")`)
+						}
+					});
+				})
+			});
+		} else {
+			con.query(`SELECT theme FROM usersthemes`, (err, data)=>{
+				const theme = data.map((el)=>{
+					return el.theme
+				})
+				theme.forEach((elemen,i)  => {
+					if (elemen != theme[i]) {
+						theme.forEach(element => {
+							console.log('addr')
+							con.query(`INSERT INTO usersthemes (course, theme) VALUE("${reqArr[1]}", "${element}")`)
+						});
+					}
+				});
+			})
+			
+		}
+	})
+	res.setHeader('Content-Type', 'application/json');
+	res.send(['redirect'])
+})
+
+app.post('/givelesson', (req, res) =>{
+	const reqArr = req.body.split(',')
+	con.query(`SELECT theme FROM usersthemes`, (err, data)=>{
+		const theme = data.map((el)=>{
+			return el.theme
+		})
+		const selecttheme = theme[0]
+		con.query(`SELECT lesson FROM lessons WHERE theme="${selecttheme}"`, (err, data)=>{
+			const lesson = data.map((el)=>{
+				return el.lesson
+			})
+			res.setHeader('Content-Type', 'application/json');
+			res.send(lesson)
+		})
+	})
+
+	
+})
+
+
 
 const server = app.listen(port, () => {
 	console.log('Server is up and running on port ' + port)
